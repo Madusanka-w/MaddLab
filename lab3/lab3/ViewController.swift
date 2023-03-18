295c1364be4640873e6136c77b485ee85ae11817
Madusanka Wattaladeniya <mwattale@gmail.com>
2023-03-18 05:35:08 -0700

    Add files via upload

---
+++ lab3/lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by Madusanka Waththaladeniya on 2023-03-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
